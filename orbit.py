e797b20bbe1972e98c82ab413c379c8d9ebc46e2
Hayden Tedrake <hayden17@mit.edu>
2025-06-13 11:52:28 -0400

    visualizing orbit for different time steps

--- orbit.py
+++ orbit.py
@@ -1,35 +1,36 @@
 import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
-import gala.integrate as gi
 import gala.dynamics as gd
 import gala.potential as gp
 from gala.potential import NFWPotential
 from gala.units import galactic
 
-# define potential
+# Define potential
 pot = NFWPotential.from_M200_c(M200=1e12 * u.M_sun, c=10.0, units=galactic)
 
-# set initial conditions and specify time-stepping
-ics = gd.PhaseSpacePosition(pos=[10,0,0.] * u.kpc, vel=[0,175,0] * u.km/u.s)
-orbit = pot.integrate_orbit(ics, dt=1*u.Myr, n_steps=100)
+# Initial conditions
+ics = gd.PhaseSpacePosition(pos=[10, 0, 0] * u.kpc, vel=[0, 175, 0] * u.km/u.s)
 
-# integrating orbits in parallel
-norbits = 128
-new_pos = np.random.normal(ics.pos.xyz.to(u.pc).value, 100., size=(norbits,3)).T * u.pc
-new_vel = np.random.normal(ics.vel.d_xyz.to(u.km/u.s).value, 1., size=(norbits,3)).T * u.km/u.s
-new_ics = gd.PhaseSpacePosition(pos=new_pos, vel=new_vel)
-orbits = pot.integrate_orbit(new_ics, dt=1*u.Myr, n_steps=100)
-
-# plot the first particle
-times = np.arange(orbits.ntimes)
-pos_output = orbits[:,0].pos.xyz.value
+# Define time step values to compare
+dt_values = [1, 10, 50]  # in Myr
+colors = ['blue', 'orange', 'red']
+labels = ['dt = 1 Myr', 'dt = 10 Myr', 'dt = 50 Myr']
 
 plt.figure()
-plt.scatter(pos_output[0,:], pos_output[1,:],
-            c=times, cmap='Spectral',s=1)
-plt.colorbar()
-plt.xlabel('X Value')
-plt.ylabel('Y Value')
-plt.title('Y vs X')
+
+for dt_val, color, label in zip(dt_values, colors, labels):
+    dt = dt_val * u.Myr
+    n_steps = int(500 * u.Myr / dt)
+    orbit = pot.integrate_orbit(ics, dt=dt, n_steps=n_steps)
+
+    pos = orbit.pos.xyz.value
+    plt.scatter(pos[0], pos[1], color=color, label=label, s=5)
+
+plt.xlabel('X [kpc]')
+plt.ylabel('Y [kpc]')
+plt.title('Comparison of Orbits with Varying Time Steps')
+plt.legend()
+plt.axis('equal')
+plt.grid(True)
 plt.show()
