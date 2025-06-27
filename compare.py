47a94e141b95f3090fccecbb181919fe1b0fdf47
Hayden Tedrake <hayden17@mit.edu>
2025-06-27 17:05:56 -0400

    made new way THE way

--- compare.py
+++ compare.py
@@ -28,20 +28,6 @@ hvs5 = {
     'rv': 545.50, #km/s
     'e_rv': 4.30, #km/s
     }
-hvs5['SkyCoord'] = coord.SkyCoord(
-    ra=hvs5['ra'] * u.degree,
-    dec=hvs5['dec'] * u.degree,
-    distance=hvs5['dist'] * u.kpc,
-    pm_ra_cosdec=hvs5['pmra'] * (u.mas / u.yr),
-    pm_dec=hvs5['pmdec'] * (u.mas / u.yr),
-    radial_velocity=hvs5['rv'] * (u.km / u.s),
-    frame="icrs"
-)
-hvs5['GC'] = hvs5['SkyCoord'].transform_to(coord.Galactocentric)
-hvs5['ics'] = gd.PhaseSpacePosition(pos=hvs5['GC'].cartesian.xyz, vel=hvs5['GC'].velocity.d_xyz)
-hvs5['orbit1'] = pot.integrate_orbit(hvs5['ics'], dt=-0.1*u.Myr, n_steps=10000) # 1 Gyr
-hvs5['orbit2'] = pot.integrate_orbit(hvs5['ics'], dt=-0.1*u.Myr, n_steps=5000) # 500 Myr
-hvs5['orbit3'] = pot.integrate_orbit(hvs5['ics'], dt=-0.1*u.Myr, n_steps=1000) # 100 Myr
 hvs8 = {
     'ra': 145.5584849227, #degree
     'e_ra': 0.0001, #degree
@@ -56,20 +42,6 @@ hvs8 = {
     'rv': 499.30, #km/s
     'e_rv': 2.90, #km/s
     }
-hvs8['SkyCoord'] = coord.SkyCoord(
-    ra=hvs8['ra'] * u.degree,
-    dec=hvs8['dec'] * u.degree,
-    distance=hvs8['dist'] * u.kpc,
-    pm_ra_cosdec=hvs8['pmra'] * (u.mas / u.yr),
-    pm_dec=hvs8['pmdec'] * (u.mas / u.yr),
-    radial_velocity=hvs8['rv'] * (u.km / u.s),
-    frame="icrs"
-)
-hvs8['GC'] = hvs8['SkyCoord'].transform_to(coord.Galactocentric)
-hvs8['ics'] = gd.PhaseSpacePosition(pos=hvs8['GC'].cartesian.xyz, vel=hvs8['GC'].velocity.d_xyz)
-hvs8['orbit1'] = pot.integrate_orbit(hvs8['ics'], dt=-0.1*u.Myr, n_steps=10000) # 1 Gyr
-hvs8['orbit2'] = pot.integrate_orbit(hvs8['ics'], dt=-0.1*u.Myr, n_steps=5000) # 500 Myr
-hvs8['orbit3'] = pot.integrate_orbit(hvs8['ics'], dt=-0.1*u.Myr, n_steps=1000) # 100 Myr
 hvs14 = {
     'ra': 161.0072805184, #degree
     'e_ra': 0.0001, #degree
@@ -84,20 +56,6 @@ hvs14 = {
     'rv': 537.30, #km/s
     'e_rv': 7.20, #km/s
     }
-hvs14['SkyCoord'] = coord.SkyCoord(
-    ra=hvs14['ra'] * u.degree,
-    dec=hvs14['dec'] * u.degree,
-    distance=hvs14['dist'] * u.kpc,
-    pm_ra_cosdec=hvs14['pmra'] * (u.mas / u.yr),
-    pm_dec=hvs14['pmdec'] * (u.mas / u.yr),
-    radial_velocity=hvs14['rv'] * (u.km / u.s),
-    frame="icrs"
-)
-hvs14['GC'] = hvs14['SkyCoord'].transform_to(coord.Galactocentric)
-hvs14['ics'] = gd.PhaseSpacePosition(pos=hvs14['GC'].cartesian.xyz, vel=hvs14['GC'].velocity.d_xyz)
-hvs14['orbit1'] = pot.integrate_orbit(hvs14['ics'], dt=-0.1*u.Myr, n_steps=10000) # 1 Gyr
-hvs14['orbit2'] = pot.integrate_orbit(hvs14['ics'], dt=-0.1*u.Myr, n_steps=5000) # 500 Myr
-hvs14['orbit3'] = pot.integrate_orbit(hvs14['ics'], dt=-0.1*u.Myr, n_steps=1000) # 100 Myr
 hvs17 = {
     'ra': 250.4849449351, #degree
     'e_ra': 0.0001, #degree
@@ -112,20 +70,6 @@ hvs17 = {
     'rv': 250.20, #km/s
     'e_rv': 2.90, #km/s
     }
-hvs17['SkyCoord'] = coord.SkyCoord(
-    ra=hvs17['ra'] * u.degree,
-    dec=hvs17['dec'] * u.degree,
-    distance=hvs17['dist'] * u.kpc,
-    pm_ra_cosdec=hvs17['pmra'] * (u.mas / u.yr),
-    pm_dec=hvs17['pmdec'] * (u.mas / u.yr),
-    radial_velocity=hvs17['rv'] * (u.km / u.s),
-    frame="icrs"
-)
-hvs17['GC'] = hvs17['SkyCoord'].transform_to(coord.Galactocentric)
-hvs17['ics'] = gd.PhaseSpacePosition(pos=hvs17['GC'].cartesian.xyz, vel=hvs17['GC'].velocity.d_xyz)
-hvs17['orbit1'] = pot.integrate_orbit(hvs17['ics'], dt=-0.1*u.Myr, n_steps=10000) # 1 Gyr
-hvs17['orbit2'] = pot.integrate_orbit(hvs17['ics'], dt=-0.1*u.Myr, n_steps=5000) # 500 Myr
-hvs17['orbit3'] = pot.integrate_orbit(hvs17['ics'], dt=-0.1*u.Myr, n_steps=1000) # 100 Myr
 hvs23 = {
     'ra': 329.1209186025, #degree
     'e_ra': 0.0001, #degree
@@ -140,20 +84,6 @@ hvs23 = {
     'rv': 259.30, #km/s
     'e_rv': 9.80, #km/s
     }
-hvs23['SkyCoord'] = coord.SkyCoord(
-    ra=hvs23['ra'] * u.degree,
-    dec=hvs23['dec'] * u.degree,
-    distance=hvs23['dist'] * u.kpc,
-    pm_ra_cosdec=hvs23['pmra'] * (u.mas / u.yr),
-    pm_dec=hvs23['pmdec'] * (u.mas / u.yr),
-    radial_velocity=hvs23['rv'] * (u.km / u.s),
-    frame="icrs"
-)
-hvs23['GC'] = hvs23['SkyCoord'].transform_to(coord.Galactocentric)
-hvs23['ics'] = gd.PhaseSpacePosition(pos=hvs23['GC'].cartesian.xyz, vel=hvs23['GC'].velocity.d_xyz)
-hvs23['orbit1'] = pot.integrate_orbit(hvs23['ics'], dt=-0.1*u.Myr, n_steps=10000) # 1 Gyr
-hvs23['orbit2'] = pot.integrate_orbit(hvs23['ics'], dt=-0.1*u.Myr, n_steps=5000) # 500 Myr
-hvs23['orbit3'] = pot.integrate_orbit(hvs23['ics'], dt=-0.1*u.Myr, n_steps=1000) # 100 Myr
 
 # Dwarf galaxy
 dict = {
@@ -250,19 +180,22 @@ map = {
     4: 'hvs23',
 }
 
-# 1 Gyr in the past
-dwarf_points = []
-
-for i, orbit in enumerate(dwarf_orbits1):
-    final_pos = orbit[-1].pos.xyz.to(u.kpc).value[:3]
-    dwarf_points.append(final_pos)
-
-dwarf_points = np.array(dwarf_points)
-
-dwarf_x = dwarf_points[:, 0]
-dwarf_y = dwarf_points[:, 1]
-dwarf_z = dwarf_points[:, 2]
+# Calculate
 for num, star in enumerate([hvs5, hvs8, hvs14, hvs17, hvs23]):
+    SkyCoord = coord.SkyCoord(
+    ra=star['ra'] * u.degree,
+    dec=star['dec'] * u.degree,
+    distance=star['dist'] * u.kpc,
+    pm_ra_cosdec=star['pmra'] * (u.mas / u.yr),
+    pm_dec=star['pmdec'] * (u.mas / u.yr),
+    radial_velocity=star['rv'] * (u.km / u.s),
+    frame="icrs"
+    )
+    GC = SkyCoord.transform_to(coord.Galactocentric)
+    ics = gd.PhaseSpacePosition(pos=GC.cartesian.xyz, vel=GC.velocity.d_xyz)
+    orbit1 = pot.integrate_orbit(ics, dt=-0.1*u.Myr, n_steps=10000) # 1 Gyr
+    orbit2 = pot.integrate_orbit(ics, dt=-0.1*u.Myr, n_steps=5000) # 500 Myr
+    orbit3 = pot.integrate_orbit(ics, dt=-0.1*u.Myr, n_steps=1000) # 100 Myr
     ra_samples = np.random.normal(star['ra'], star['e_ra'], n_samples)
     dec_samples = np.random.normal(star['dec'], star['e_dec'], n_samples)
     dist_samples = np.random.normal(star['dist'], star['e_dist'], n_samples)
@@ -273,33 +206,56 @@ for num, star in enumerate([hvs5, hvs8, hvs14, hvs17, hvs23]):
     ics_list = []
     for ra, dec, dist, pmra, pmdec, rv in zip(ra_samples, dec_samples, dist_samples, pmra_samples, pmdec_samples, rv_samples):
         sc = coord.SkyCoord(ra=ra * u.degree,
-        dec=dec * u.degree,
-        distance=dist * u.kpc,
-        pm_ra_cosdec=pmra * (u.mas / u.yr),
-        pm_dec=pmdec * (u.mas / u.yr),
-        radial_velocity=rv * (u.km / u.s),
-        frame="icrs")
+                    dec=dec * u.degree,
+                    distance=dist * u.kpc,
+                    pm_ra_cosdec=pmra * (u.mas / u.yr),
+                    pm_dec=pmdec * (u.mas / u.yr),
+                    radial_velocity=rv * (u.km / u.s),
+                    frame="icrs")
         gc = sc.transform_to(coord.Galactocentric)
         ics = gd.PhaseSpacePosition(pos=gc.cartesian.xyz, vel=gc.velocity.d_xyz)
         ics_list.append(ics)
-            
-    orbits = []
-    orbits.append(orbit)
+
+    orbits1 = []
+    orbits2 = []
+    orbits3 = []
+    orbits1.append(orbit1)
+    orbits2.append(orbit2)
+    orbits3.append(orbit3)
     for ics in ics_list:
-        orbit = pot.integrate_orbit(ics, dt=-0.1*u.Myr, n_steps=10000)
-        orbits.append(orbit)
-
+        orbit1 = pot.integrate_orbit(ics, dt=-0.1*u.Myr, n_steps=10000)
+        orbit2 = pot.integrate_orbit(ics, dt=-0.1*u.Myr, n_steps=5000)
+        orbit3 = pot.integrate_orbit(ics, dt=-0.1*u.Myr, n_steps=1000)
+        orbits1.append(orbit1)
+        orbits2.append(orbit2)
+        orbits3.append(orbit3)
+        
     points = []
-
-    for i, orbit in enumerate(orbits):
-        final_pos = orbits[-1].pos.xyz.to(u.kpc).value[:3]
-        points.append(final_pos)
+    for i, orbit in enumerate(orbits1):
+        final_pos = orbit[-1].pos.xyz.to(u.kpc).value[:3]
+        if i == 0:
+            pass
+        else:
+            points.append(final_pos)
 
     points = np.array(points)
 
     x = points[:, 0]
     y = points[:, 1]
     z = points[:, 2]
+
+    # 1 Gyr
+    dwarf_points = []
+
+    for i, orbit in enumerate(dwarf_orbits1):
+        final_pos = orbit[-1].pos.xyz.to(u.kpc).value[:3]
+        dwarf_points.append(final_pos)
+
+    dwarf_points = np.array(dwarf_points)
+
+    dwarf_x = dwarf_points[:, 0]
+    dwarf_y = dwarf_points[:, 1]
+    dwarf_z = dwarf_points[:, 2]
 
     XY = compare(dwarf_x, dwarf_y, x, y)
     distance_xy = np.sqrt(XY[0]**2 + XY[1]**2)
